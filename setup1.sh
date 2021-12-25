6eceae062f24c824f11466c95912ae0b41b13986
Mateusz Konieczny <matkoniecz@gmail.com>
2021-12-25 01:17:55 +0100

    upgrade to postgresql-12-postgis-3

--- setup1.sh
+++ setup1.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y build-essential git vim-nox wget curl
 sudo apt-get install -y python3 python3-pip python3-dev python3-venv
-sudo apt-get install -y postgresql postgresql-contrib libpq-dev postgis postgresql-10-postgis-2.4
+sudo apt-get install -y postgresql postgresql-contrib libpq-dev postgis postgresql-12-postgis-3
 sudo apt-get install -y gdal-bin libspatialindex-dev libgeos-dev libproj-dev
 git clone https://github.com/colouring-london/colouring-london.git
 export NODE_VERSION=v12.14.1
