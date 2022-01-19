66e9c477bc21b3264f50d82e608934de787f9b19
Mateusz Konieczny <matkoniecz@gmail.com>
2022-01-19 23:33:58 +0100

    reduce spam of the script

--- setup1.sh
+++ setup1.sh
@@ -10,11 +10,11 @@ trap 'err_report $LINENO' ERR
 # Setting up a local development environment
 
 sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y build-essential git vim-nox wget curl
-sudo apt-get install -y python3 python3-pip python3-dev python3-venv
-sudo apt-get install -y postgresql postgresql-contrib libpq-dev postgis postgresql-12-postgis-3
-sudo apt-get install -y gdal-bin libspatialindex-dev libgeos-dev libproj-dev
+sudo apt-get upgrade -y --quiet
+sudo apt-get install -y --quiet build-essential git vim-nox wget curl
+sudo apt-get install -y --quiet python3 python3-pip python3-dev python3-venv
+sudo apt-get install -y --quiet postgresql postgresql-contrib libpq-dev postgis postgresql-12-postgis-3
+sudo apt-get install -y --quiet gdal-bin libspatialindex-dev libgeos-dev libproj-dev
 cd ~/Desktop
 mkdir outer_colouring_london_folder
 cd outer_colouring_london_folder
