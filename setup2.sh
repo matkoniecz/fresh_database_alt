31cce2630f431256ad2e0a26b3c74b6157798432
Mateusz Konieczny <matkoniecz@gmail.com>
2022-01-19 10:23:56 +0100

    fix broken folder

--- setup2.sh
+++ setup2.sh
@@ -23,8 +23,8 @@ if [[ -e ~/.profile ]] ; then
 fi
 service postgresql start
 sudo locale-gen en_US.UTF-8
-sudo sed -i "s/#\?listen_address.*/listen_addresses '*'/" /etc/postgresql/10/main/postgresql.conf
-echo "host    all             all             all                     md5" | sudo tee --append /etc/postgresql/10/main/pg_hba.conf > /dev/null
+sudo sed -i "s/#\?listen_address.*/listen_addresses '*'/" /etc/postgresql/12/main/postgresql.conf
+echo "host    all             all             all                     md5" | sudo tee --append /etc/postgresql/12/main/pg_hba.conf > /dev/null
 service postgresql restart
 sudo -u postgres psql -c "SELECT 1 FROM pg_user WHERE usename = '$USER';" | grep -q 1 || sudo -u postgres psql -c "CREATE ROLE $USER SUPERUSER LOGIN PASSWORD 'password';"
 sudo -u postgres psql -c "SELECT 1 FROM pg_database WHERE datname = 'colouringlondondb';" | grep -q 1 || sudo -u postgres createdb -E UTF8 -T template0 --locale=en_US.utf8 -O $USER colouringlondondb
